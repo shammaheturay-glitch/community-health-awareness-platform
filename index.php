de757bb791627018d6b09bb5b51faa40744f3f05
Aaron-Disigner <aaronromeo14@gmail.com>
2026-06-15 13:05:33 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+include 'includes/header.php';
+?>
+
+<h2>Welcome to the Community Health Awareness Platform</h2>
+
+<p>
+This platform provides reliable health information and disease prevention
+guidance for communities in Sierra Leone.
+</p>
+
+<h3>Featured Diseases</h3>
+
+<ul>
+    <li>Malaria</li>
+    <li>Cholera</li>
+    <li>Typhoid</li>
+    <li>Diabetes</li>
+    <li>Hypertension</li>
+</ul>
+
+<h3>Health Awareness Mission</h3>
+
+<p>
+Our goal is to improve public awareness about common diseases and promote
+healthy living practices through accessible digital information.
+</p>
+
+<?php
+include 'includes/footer.php';
+?>
